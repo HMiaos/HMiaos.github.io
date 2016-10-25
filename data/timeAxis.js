baa6b137c461d3537215a7ade67af896f066ff78
HMiaos <695207869@qq.com>
2016-10-25 14:07:35 +0800

    10-25-first

--- data/timeAxis.js
+++ data/timeAxis.js
@@ -15,24 +15,4 @@
 */
 
 var timeAxisArr = [
-		{
-		    startTime:"2016.03.10",
-			endTime:"至今",
-			companyLogo:'images/icon/expre_logo4.png',
-			companyNam:"秒味课堂",
-			companyWebsite:"http://www.miaov.com",
-			jobPost:"前端讲师助理",
-			jonTask:"参与搭建了公司前端的整体构架；负责平时网站维护和信息管理",
-			jobContent:"包括：制作前端静态资源结构；html与css课程讲解；整理学员考勤和学习信息；学员问题的及时解答"
-		},
-		{
-		    startTime:"2014.10.01",
-			endTime:"2015.9.16",
-			companyLogo:'images/icon/expre_logo5.png',
-			companyNam:"中铁六局太原铁路建设有限公司建安项目部",
-			companyWebsite:"http://www.miaov.com",
-			jobPost:"实习生",
-			jonTask:"测量工程数据；核定公司设计方案",
-			jobContent:"包括：实地测量数据，验证数据，核定方案，维护；根据工程技术部安排完成描图，晒图任务；根据提供资料，设计方案，草图和技术说明绘制正图和底图；设计资料的装订和维护工作",
-		}
 ]
